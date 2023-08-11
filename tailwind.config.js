45595ef120b8ceb35bb5298b14e2b7889ea58811
nooralamf42 <nooralamf42@gmail.com>
2023-08-11 17:53:10 +0530

    almost completed day-4 tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       },
       backgroundImage : {
         biggestDiv : "url('https://razorpay.com/build/browser/static/x-dark-bg.0bc3bc00.svg')"
+      },
+      animation : {
+        scroll : "scroll 25s linear infinite reverse"
+      },
+      keyframes : {
+        scroll : {
+          "0%": {
+            "transform" : "translateY(-100%)"
+          },
+          "100%" : {
+            "transfrom" : "translateY(0%)"
+          }
+        }
       }
     },
   },
